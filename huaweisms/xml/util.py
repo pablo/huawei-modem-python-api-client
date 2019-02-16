3d74e333eab6980f5ad003833139ffb9cd37545c
Mka Madlavana <mkhanyisi.madlavana@hpe.com>
2019-02-17 01:03:37 +0200

    cleanup

--- huaweisms/xml/util.py
+++ huaweisms/xml/util.py
@@ -55,47 +55,6 @@ def get_dictionary_from_children(elem: Element):
 def parse_xml_string(xmlString: str) -> Document:
     return minidom.parseString(xmlString)
 
-class DictToXML(object):
-
-    def __init__(self, structure, list_mappings={}):
-        self.doc = Document()
-
-        if len(structure) == 1:
-            rootName = str(list(structure.keys())[0])
-            self.root = self.doc.createElement(rootName)
-
-            self.list_mappings = list_mappings
-
-            self.doc.appendChild(self.root)
-            self.build(self.root, structure[rootName])
-
-    def build(self, father, structure):
-        if type(structure) == dict:
-            for k in structure:
-                tag = self.doc.createElement(k)
-                father.appendChild(tag)
-                self.build(tag, structure[k])
-        elif type(structure) == list:
-            tag_name = self.default_list_item_name
-
-            if father.tagName in self.list_mappings:
-                tag_name = self.list_mappings[father.tagName]
-
-            for l in structure:
-                tag = self.doc.createElement(tag_name)
-                self.build(tag, l)
-                father.appendChild(tag)
-        else:
-            data = str(structure)
-            tag = self.doc.createTextNode(data)
-            father.appendChild(tag)
-
-    def display(self):
-        print(self.doc.toprettyxml(indent="  "))
-
-    def get_string(self):
-        return self.doc.toprettyxml(indent="  ")
-
 
 def dict_to_xml(data: dict) -> str:
     if not data:
@@ -109,8 +68,6 @@ def dict_to_xml(data: dict) -> str:
                 add_children(doc, child, v)
         elif isinstance(input_data, (list, tuple)):
             for item in input_data:
-                # child = doc.createElement(parent.tagName)
-                # parent.appendChild(child)
                 add_children(doc, parent, item)
         else:
             child = doc.createTextNode(str(input_data))
