b564a22de49539af2fb13615e936ee733c8c8c1d
Mka Madlavana <mkhanyisi.madlavana@hpe.com>
2019-03-10 21:02:23 +0200

    more tests

--- tests/test_api_common.py
+++ tests/test_api_common.py
@@ -3,16 +3,31 @@
 
 import unittest
 
+import faker
 
 import huaweisms.api.common
 
 
 class ApiCommonTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.fake = faker.Factory.create()
 
     def test_common_headers(self):
         expected = {"X-Requested-With": "XMLHttpRequest"}
         data = huaweisms.api.common.common_headers()
         self.assertDictEqual(data, expected)
+
+    def test_build_cookies(self):
+        ctx = huaweisms.api.common.ApiCtx()
+        cookies = huaweisms.api.common.build_cookies(ctx)
+        self.assertIsNone(cookies)
+
+        fake_id = self.fake.sha256()
+        ctx.session_id = fake_id
+        cookies = huaweisms.api.common.build_cookies(ctx)
+        self.assertIsInstance(cookies, dict)
+        self.assertDictEqual(cookies, {'SessionID': fake_id})
 
 
 if __name__ == '__main__':
